f52d00f1e35ab6b744b68ef3b60175c8c7cee767
Nurillo Mahmudjonov <72136850+iCloudmrx@users.noreply.github.com>
2023-05-11 11:56:01 +0500

    update setting.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ponsj^s25!1bn^c#(vzd%mo1!k+p2@(ys9*$t9r06xe_-lrnm+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.railway.app', '127.0.0.1']
 
 
 # Application definition
